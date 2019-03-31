b99a29e9f82244eef3cb7d81e8017a3bca3ccbfc
huruji <huruji3@foxmail.com>
2019-03-31 23:13:25 +0800

    feat: js => ts

--- src/preloader.ts
+++ src/preloader.ts
@@ -1,5 +1,22 @@
+export interface EType {
+  progress: Function[],
+  error: Function[]
+}
+
+const EVENT_PROGRESS:string = 'progress'
+const EVENT_ERROR:string = 'error'
+const EVENT_LOAD:string = 'load'
+
+const FUNCTION_TYPE = 'function'
+
 export default class Loader {
-  constructor(assets) {
+  assets: Array<string>
+  loadedNum: number
+  events: EType
+  onerror: Function
+  onprogress: Function
+
+  constructor(assets: string | string[]) {
     this.assets = Array.isArray(assets) ? assets : [assets]
     this.loadedNum = 0
     this.events = {
@@ -16,24 +33,24 @@ export default class Loader {
     const promiseLists = assets.map(asset => new Promise((resolve) => {
       const img = new Image()
       img.src = asset
-      img.addEventListener('load', () => {
+      img.addEventListener(EVENT_LOAD, () => {
         self.loadedNum++
         const progress = self.loadedNum / assets.length * 100
-        self.emit('progress', progress)
-        if (self.onprogress && typeof self.onprogress === 'function') {
+        self.emit(EVENT_PROGRESS, progress)
+        if (self.onprogress && typeof self.onprogress === FUNCTION_TYPE) {
           self.onprogress.call(this, progress)
         }
         resolve(img)
       })
-      img.addEventListener('error', (e) => {
+      img.addEventListener(EVENT_ERROR, (e) => {
         self.loadedNum++
         const progress = self.loadedNum / assets.length * 100
-        self.emit('error', e)
-        if (self.onerror && typeof self.onerror === 'function') {
+        self.emit(EVENT_ERROR, e)
+        if (self.onerror && typeof self.onerror === FUNCTION_TYPE) {
           self.onerror.call(this, e)
         }
-        self.emit('progress', progress)
-        if (self.onprogress && typeof self.onprogress === 'function') {
+        self.emit(EVENT_PROGRESS, progress)
+        if (self.onprogress && typeof self.onprogress === FUNCTION_TYPE) {
           self.onprogress.call(this, progress)
         }
         resolve(img)
@@ -43,17 +60,21 @@ export default class Loader {
     return Promise.all(promiseLists)
   }
 
-  listen(name, cb) {
+  listen(name:string, cb:Function):void{
     if (!Object.keys(this.events).includes(name)) return
     this.events[name].push(cb)
   }
 
-  emit(name, ...args) {
+  emit(name:string, ...args:any[]):void {
     if (!Object.keys(this.events).includes(name)) return
     for (let i = 0; i < this.events[name].length; i++) {
       this.events[name][i].apply(this, args)
     }
   }
-}
 
-Loader.prototype.addEventListener = Loader.prototype.listen
+  add(asset:string) {
+    this.assets.push(asset)
+  }
+
+  addEventListener = this.listen
+}
